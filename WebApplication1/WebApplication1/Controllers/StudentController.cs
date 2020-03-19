be471800d2e7ed73277602350e9c68d6d216f98b
IziKodzik <ITI-guuy@gmail.com>
2020-03-19 23:58:02 +0100

    po zajeciach

---
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public string getStudents(String orderBy)
+        {
+
+
+            String result = $"DZBAN, drugi, Andrzejweski sort={orderBy}";
+            return result;
+
+        }
+        
+
+        [HttpGet("{id}")]
+        public IActionResult getStudent(int id)
+        {
+            if(id == 1)
+                return Ok("DZBAN");
+            if (id == 2)
+                return Ok("drugi");
+            if (id == 3)
+                return Ok("trzeci");
+            else
+                return NotFound("Wrong id");
+        }
+    }
+}
